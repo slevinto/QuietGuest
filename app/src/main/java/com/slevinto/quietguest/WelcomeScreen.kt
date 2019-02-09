bbb14544040ecd24d99be5d1078badda14ba18b6
slevinto <slevinto@gmail.com>
2019-02-09 16:09:48 +0200

    add welcome screen and style

---
+++ app/src/main/java/com/slevinto/quietguest/WelcomeScreen.kt
@@ -0,0 +1,40 @@
+package com.slevinto.quietguest
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class WelcomeScreen : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private val welcomeScreenDelay: Long = 3000 //3 seconds
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+        //Initialize the Handler
+        mDelayHandler = Handler()
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, welcomeScreenDelay)
+    }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+
+}
